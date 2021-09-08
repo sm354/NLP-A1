a8a76b2e118265ff07b4d952211ddaf068bc147b
sm354 <shubhamiitd18@gmail.com>
2021-09-09 00:38:46 +0530

    num2words rule added (some edge cases remaining)

--- run_assignment1.py
+++ run_assignment1.py
@@ -14,38 +14,8 @@ Rule-based system
     -----
     silent tokens (sil)
 
-    abbreviations
-        All abbreviations are separated as they are spoken. Example, “U.S.” or “US” is converted to “u s”
-
-    dates
-        All dates are converted into words. Example, “29 March 2012” will be converted to “the
-        twenty ninth of march twenty twelve”. “2011-01-25” will be converted to “the twenty fifth of
-        january twenty eleven”
-
-    times
-        All times are converted into words. Example, “04:40 PM” is converted to “four forty p m”.
-        “21:30:12” is converted to “twenty one hours thirty minutes and twelve seconds”
-
-    numeric quanities
-        All numeric quantities are converted to words using Western numbering system. Example,
-        “10345” is converted to “ten thousand three hundred forty five”. “24349943” is converted to
-        “twenty four million three hundred forty nine thousand nine hundred forty three”. “184.33” is
-        converted to “one hundred eighty four point three three”. “-20” is converted to “minus twenty”.
-
-    units
-        All units are spelled out as spoken. “53.77 mm” is converted to “fifty three point seven
-        seven millimeters”, “3 mA” is converted to “three milliamperes”, and “14 sq m” is converted to
-        “fourteen square meters”.
-
-    currency
-        Currency is also spelled out. “$15.24” is converted to “fifteen dollars and twenty four cents”.
-        “£11” is converted to “eleven pounds”.  
-
-Appendix
---------
-    converting honorifics - Mr Mrs Dr ? probably no
+    converting honorifics - Mr Mrs Dr ? probably NO
 '''
-
 
 import argparse
 import json
@@ -76,7 +46,7 @@ def findOT(inp_tok, rules):
 def solution(input_tokens):
     output_tokens = []
 
-    rules = [sil, abbreviation, ]
+    rules = [sil, abbreviation, dates2words, time2words, num2words, units2words, currency2words]
 
     for input_token in input_tokens:
         output_token = findOT(input_token, rules)
@@ -128,16 +98,157 @@ def abbreviation(string):
     return out.strip()
 
 def dates2words(string):
-
-    pass
+    '''
+    possible formats
+    ----------------
+        29 March 2012
+        2011-01-25
+        March 1921
+    '''
+    # first find year
+    regex_year = r"[1-2]\d{3}"
+    match = re.search(regex_year, string)
+    
+    if match == None:
+        return None
+    out = ""
+    
+    year = match.group()
+    # 1921 -> nineteen twenty one
+    
+    return out
 
 def time2words(string):
 
     pass
 
 def num2words(string):
+    '''
+    possible formats
+    ----------------
+        num
+            123141512, 0, 199, -123
+        comma_num
+            12,000 twelve thousand
+        dec_perc_num
+            123.123
+            90.0 ninety point zero
+            0.63% zero point six three percent
+            99.05% ninety nine point o five percent
+            0.32% zero point three two percent
+            IMPORTANT ------- 1,565.0 one thousand five hundred sixty five point zero
+            25.520 twenty five point five two o
+            13.0088 thirteen point o o eight eight
+        frac_num    
+            7/283 : seven two hundred eighty thirds
+            1 1/2 one and a half
+        hyp_num
+            978-0-304-35252-4 nine seven eight sil o sil three o four sil three five two five two sil four
+            1-881089-97-5 one sil eight eight one o eight nine sil nine seven sil five
+            0-8387-1972-4 o sil eight three eight seven sil one nine seven two sil four
+    '''
+    regex_num = r"(-?)\d+"
+    match = re.fullmatch(regex_num, string)
+    if match != None:
+        return _number_to_word(match.group())
+    
+    regex_comma_num = r"(-?)((,?)\d{1,3})+" # this includes some false weird examples also like '90,0', ',123,1,1', etc
+    match = re.fullmatch(regex_comma_num, string)
+    if match != None:
+        return _number_to_word(''.join(match.group().split(',')))
 
-    pass
+    # regex_dec_perc = r"(-?)(\d*)(\.)(\d)" # this includes some false weird examples also like '90,0', ',123,1,1', etc
+    # match = re.fullmatch(regex_dec_perc, string)
+    
+    regex_hyp_num = r"(\d+-)+(\d+)" # this includes dates also -- NEED TO FIX THIS IMPORTANT!
+    match = re.fullmatch(regex_hyp_num, string)
+    if match != None:
+        return _hyphen_num_to_word(match.group())
+
+    return None
+
+def _number_to_word(number):
+    '''
+    This will convert the given number (in any range) string to word string
+    Numbers in [0,20] are instantly converted using _num2word
+    Others are passed through an algorithm that splits the number in groups of 3
+
+    '''
+    negative_num = False
+    if number[0] == '-':
+        negative_num = True
+        number = number[1:]
+        
+    if 0 <= int(number) <= 20:
+        # this takes care of case when number == 0 (empty string is not returned)
+        # -0 is returned as 0
+        return _num2word[int(number)]
+    
+    final_word = []
+    
+    # now split the number in groups of 3 from last
+    grps_of_3digits = []
+    i = len(number)-3
+    while i>=0:
+        grps_of_3digits.append(number[i:i+3])
+        i-=3
+    if i+3 != 0: # avoid empty string insertion
+        grps_of_3digits.append(number[0:i+3])
+    
+    # now convert each group one by one
+    for i, num_3dig in enumerate(grps_of_3digits):
+        num_3dig_wrd = _3dig_num2wrd(num_3dig)
+        if num_3dig_wrd != '': # handles 1,000,000 -> the last two groups should not add _placeValue
+            num_3dig_wrd += (" " + _placeValue[i])
+        num_3dig_wrd = num_3dig_wrd.strip()
+        
+        if num_3dig_wrd != '':
+            final_word.insert(0, num_3dig_wrd)
+    
+    if negative_num:
+        final_word.insert(0, "minus")
+    
+    final_word = ' '.join(final_word)
+    return final_word
+
+def _3dig_num2wrd(num):
+    '''
+    this accepts num in [0, 999]
+    empty string returned if int(num) is 0 
+    
+    there are three cases
+        0 <= num <= 20 or num == 30, 40, ..., 90 --> these are hard coded
+        20 < num < 100 and num != 30, 40, ..., 90
+        100 <= num < 1000 --> this also gets split into cases
+    '''
+    num = int(num)
+    final_word = None
+    if num == 0:
+        final_word = ""
+    
+    elif 0 < num <= 20 or (num%10 == 0 and num < 100): 
+        final_word = _num2word[num]
+    
+    elif 20 < num < 100:
+        tenths = 10 * (num // 10)
+        ones = num % 10
+        final_word = _num2word[tenths] + " " + _num2word[ones]
+    
+    else:
+        hundredths = num//100
+        final_word = _num2word[hundredths] + " hundred " + _3dig_num2wrd(str(num%100))
+    return final_word.strip()
+
+def _hyphen_num_to_word(hyph_num):
+    final_word = []
+    for ch in hyph_num:
+        if ch == '-':
+            final_word.append('sil') 
+        elif ch == '0':
+            final_word.append('o')
+        else:
+            final_word.append(_num2word[int(ch)])
+    return ' '.join(final_word)
 
 def units2words(string):
 
@@ -147,10 +258,34 @@ def currency2words(string):
 
     pass
 
+def _make_vocab():
+    # make dictionaries for hard-coded mappings like '1' -> 'one'
+
+    global _num2word
+    _num2word = {}
+    _words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    _words+= ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    for i in range(20):
+        _num2word[i] = _words[i]
+    _words = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+    for i in range(20, 100, 10):
+        _num2word[i] = _words[i//10 - 2]
+    
+    global _placeValue
+    _placeValue = {}
+    _list = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion']
+    for i in range(len(_list)):
+        _placeValue[i] = _list[i]
+        
+    # print(_num2word)
+    # print(_placeValue)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='COL 772 Assignment 1 | 2018EE10957')
     parser = add_args(parser)
     args = parser.parse_args()
+    
+    _make_vocab()
 
     solution_dump(args)
 
@@ -166,11 +301,5 @@ if __name__ == "__main__":
 '''
 References
 ----------
-Regular expressions
-    https://www.geeksforgeeks.org/regular-expression-python-examples-set-1/
-    https://www.geeksforgeeks.org/regular-expressions-python-set-1-search-match-find/
-    https://docs.python.org/3/library/re.html
 
 '''
-
-
